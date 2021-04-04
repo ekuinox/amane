7b9c8b0b78cc595e9882228f290ee068915a3884
ekuinox <depkey@me.com>
2021-04-04 17:55:55 +0900

    ファイル分け, あとファイルの取得が行えるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,13 @@
-use std::io::Write;
-use actix_web::{App, HttpResponse, Error, HttpServer, Responder, get, web};
-use actix_multipart::Multipart;
-use futures::{StreamExt, TryStreamExt};
+mod routes;
 
-#[get("/{id}/{name}")]
-async fn index(web::Path((id, name)): web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", name, id)
-}
-
-async fn put_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
-    // iterate over multipart stream
-    while let Ok(Some(mut field)) = payload.try_next().await {
-        let content_type = field.content_disposition().unwrap();
-        let filename = content_type.get_filename().unwrap();
-        println!("{:?}", filename);
-        let filepath = format!("./tmp/{}", sanitize_filename::sanitize(&filename));
-
-        // File::create is blocking operation, use threadpool
-        let mut f = web::block(|| std::fs::File::create(filepath))
-            .await
-            .unwrap();
-
-        // Field in turn is stream of *Bytes* object
-        while let Some(chunk) = field.next().await {
-            let data = chunk.unwrap();
-            // filesystem operations are blocking, we have to use threadpool
-            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
-        }
-    }
-    Ok(HttpResponse::Ok().into())
-}
+use actix_web::{App, HttpServer};
+use routes::*;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
     HttpServer::new(|| App::new()
-        .service(
-            web::resource("/")
-            .route(web::post().to(put_file))
-        )
+        .service(put_file)
+        .service(get_file)
     )
         .bind("0.0.0.0:8080")?
         .run()
---
+++ src/routes.rs
@@ -0,0 +1,62 @@
+use actix_web::{Responder, get, put, web, HttpResponse};
+use actix_multipart::Multipart;
+
+// ファイルの保存先ディレクトリ
+const TARGET_DIRECTORY: &'static str = "./tmp";
+
+/// ファイルのパスを取得する
+fn get_path(key: &String) -> String {
+    format!(
+        "{}/{}",
+        TARGET_DIRECTORY,
+        sanitize_filename::sanitize(&key)
+    )
+}
+
+/// ファイルを取得するAPI
+#[get("/{bucket_name}/{key}")]
+async fn get_file(web::Path((_bucket_name, key)): web::Path<(String, String)>) -> impl Responder {
+    use std::io::Read;
+    let filepath = get_path(&key);
+    let file = match web::block(|| std::fs::File::open(filepath)).await {
+        Ok(f) => f,
+        Err(_) => return HttpResponse::NotFound().finish(),
+    };
+    let mut bytes: Vec<u8> = vec![];
+    for byte in file.bytes() {
+        match byte {
+            Ok(byte) => bytes.push(byte),
+            Err(_) => return HttpResponse::InternalServerError().finish(),
+        }
+    }
+    HttpResponse::Ok().body(bytes)
+}
+
+/// ファイルを更新するAPI
+#[put("/{bucket_name}/{key}")]
+async fn put_file(mut payload: Multipart, web::Path((_bucket_name, key)): web::Path<(String, String)>) -> impl Responder {
+    use futures::{StreamExt, TryStreamExt};
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let filepath = get_path(&key);
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            use std::io::Write;
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use threadpool
+            match web::block(move || f.write_all(&data).map(|_| f)).await {
+                Ok(a) => { f = a; },
+                Err(_) => {
+                    return HttpResponse::InternalServerError().finish();
+                },
+            }
+        }
+    }
+    // 別に Ok とは限らないが...
+    HttpResponse::Ok().finish()
+}
