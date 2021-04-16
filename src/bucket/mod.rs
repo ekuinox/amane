236503075652106cede0d37a6c3f1e1dba496ffa
ekuinox <depkey@me.com>
2021-04-17 03:31:25 +0900

    remove unused

--- src/bucket/mod.rs
+++ src/bucket/mod.rs
@@ -2,23 +2,6 @@ mod attributes;
 mod bucket;
 mod accessor;
 
-use sha2::{Sha256, Digest};
 pub use self::bucket::{Bucket, BucketError};
 pub use self::attributes::is_users_meta_key;
 pub use self::accessor::*;
-
-/// 文字列をハッシュ化して16進数文字列にする
-fn get_hex(value: &String) -> String {
-    let mut hasher = Sha256::new();
-    hasher.update(sanitize_filename::sanitize(value));
-    let hashed = hasher.finalize();
-    hashed.iter().map(|c| format!("{:02x}", c)).collect()
-}
-
-/// ファイルのパスを取得する
-fn get_path(data_directory: &String, bucket_name: &String, key: &String) -> String {
-    let bucket_name = get_hex(bucket_name);
-    let key = get_hex(key);
-    // とりあえずアンスコで繋げているが良いとは思えない...
-    format!("{}/{}_{}", data_directory, bucket_name, key)
-}
